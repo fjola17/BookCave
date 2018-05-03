1fd21027ef44241588f9de0ddcebff3c6ce18974
Viðar <vidarsigurds@gmail.com>
2018-05-03 14:50:05 +0000

    BookViewModel
    Hér er BookViewModel

---
+++ Models/ViewModels/BookViewModel.cs
@@ -0,0 +1,16 @@
+namespace BookCave.Models.ViewModels
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int PublishingYear { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public double Price { get; set; }
+        public string Formats { get; set; }
+        public string AudioSample { get; set; }
+        public string CoverImage { get; set; }
+    }
+}

5334de545913db142d769e1035f61205714b83b2
vidars17 <34029273+vidars17@users.noreply.github.com>
2018-05-03 19:54:54 +0000

    Smá bætur á Controllers og Repos
    Bætti Controllera og Repos sem voru með villur og kommentaði föll.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -24,6 +24,7 @@ namespace BookCave.Controllers
         public IActionResult UpdateBook(BookViewModel updatedBook)
         {
             //Þarf að útfæra betur
+            return View();
         }
     }
 }

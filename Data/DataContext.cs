81a7ee7ef4253d507de6a8e8b55496279310b155
Viðar <vidarsigurds@gmail.com>
2018-05-04 11:37:10 +0000

    Forsíðan "virkar" og Seed fallið er kommentað út

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -7,7 +7,6 @@ namespace BookCave.Data
     {
         public DbSet<Book> Books{ get; set; }
         public DbSet<Review> Reviews{ get; set; }
-
         public DbSet<Order> Orders{ get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)

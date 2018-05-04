fad9c38f5b8bd3a8a91679aa6a36133a832e8725
fjola17 <34029285+fjola17@users.noreply.github.com>
2018-05-04 21:28:15 +0000

    BookController

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -26,16 +26,29 @@ namespace BookCave.Controllers
             var books = _bookServices.GetBookIndex();
             return View(books);
         }
-        public IActionResult Details(int id) //Nær í details fyrir eina bók með id
+        public IActionResult Details(int? id) //Nær í details fyrir eina bók með id
         {
+            if(id == null) //Gleimir að skrá inn bók þarf að höndla
+            {
+                return View("Error");
+            }
             var book = _bookServices.GetById(id);
+            if(book == null) //bók finnst ekki, þarf að höndla villurnar
+            {
+                return View("Error");
+            }
             return View(book);
         }
-        public IActionResult SearchBooks(string searchString) //Nær í bækur eftir searchstring en bara eftir titli núna
+        public IActionResult SearchBooks(string searchString) //Nær í bækur eftir searchstring en bara eftir titli núna, virkar ekki!!
                                                         ///þarf að útfæra með fleiri eigindi
         {
+            if(searchString == null)
+            {
+                return View("Index"); //birtir allar bækurnar ef ítt er á search án þess að skrifa neitt
+            }
             var booksFound = _bookServices.GetBySearchString(searchString);
             return View(booksFound);
         }
+
     }
 }

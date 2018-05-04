fabd48778c8d3997496525343bed257a40aff347
fjola17 <34029285+fjola17@users.noreply.github.com>
2018-05-04 13:36:22 +0000

    Tek út Author, orderrepo getbyId "virkar"

--- Models/ViewModels/AuthorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BookCave.Models.ViewModels
-{
-    public class Author
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
-}

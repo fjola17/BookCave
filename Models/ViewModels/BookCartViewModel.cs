bcd29296b7a38ec050b620fd3f79ff1d3da4e884
fjola17 <34029285+fjola17@users.noreply.github.com>
2018-05-08 15:58:26 +0000

    Nokkur View models

---
+++ Models/ViewModels/BookCartViewModel.cs
@@ -0,0 +1,10 @@
+namespace BookCave.Models.ViewModels
+{
+    public class BookCartViewModel
+    {
+        public string Title { get; set; }
+        public string CoverImage { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+    }
+}

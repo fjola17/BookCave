82eb684a7a74143a25a14aa6a99f04bf61029a2f
Bjossidan <34061953+Bjossidan@users.noreply.github.com>
2018-05-09 13:56:12 +0000

    PROFILE
    PROFILE WORK

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -14,11 +14,7 @@ namespace BookCave.Controllers
     public class ReviewController : Controller
     {
         private ReviewRepo _reviewServices;
-<<<<<<< HEAD
-       public ReviewController()
-=======
         public ReviewController()
->>>>>>> 09b70946aa863c237674e6614ba15430be5c73ee
         {
             _reviewServices = new ReviewRepo();
         }
@@ -26,16 +22,29 @@ namespace BookCave.Controllers
         public IActionResult BookReviews(int bookId) //Nær í reviews fyrir bók
         {
             var reviewsByBookId = _reviewServices.GetByBookId(bookId);
+            if(reviewsByBookId == null)
+            {
+                return View();
+            }
             return View(reviewsByBookId);
         }
         public IActionResult UserReviews(string ownerId) //Nær í review eftir notanda
         {
+
             var reviewsByOwnerId = _reviewServices.GetByOwnerId(ownerId);
+            if(reviewsByOwnerId == null)
+            {
+                return View();
+            }
             return View(reviewsByOwnerId);
         }
         public IActionResult SingleReview(int reviewID) //Nær í ákveðið review(nánari upplýsingar)
         {
             var reviewsById = _reviewServices.GetByReviewId(reviewID);
+            if(reviewID == 0)
+            {
+                return View();
+            }
             return View(reviewsById);
         }
 

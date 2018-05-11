18927edac7bb96663fc28699e93b8115b69f6994
Viðar <vidarsigurds@gmail.com>
2018-05-11 15:26:39 +0000

    Register fix

--- Models/ViewModels/RegisterViewModel.cs
+++ Models/ViewModels/RegisterViewModel.cs
@@ -9,7 +9,7 @@ namespace BookCave.Models.ViewModels
         public string UserName { get; set; }
         [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Invalid password. Must contain Uppercase [ABC], lowercase [abc], numerical[123] and special characters [!?#].")]
+        [Required(ErrorMessage = "Invalid password.")]
         public string Password { get; set; }
         public string Image { get; set; }
         
